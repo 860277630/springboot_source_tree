89faa12962da4a6a960cf5ae956ac73624bd9389
860277630 <860277630@qq.com>
2023-05-28 00:27:46 +0800

    添加内容11

---
+++ src/main/java/com/example/springboot_source_tree/test/test02.java
@@ -0,0 +1,13 @@
+package com.example.springboot_source_tree.test;
+
+/**
+ * @program: springboot_source_tree
+ * @description:
+ * @author: wjl
+ * @create: 2023-05-28 00:26
+ **/
+public class test02 {
+    public static void main(String[] args) {
+        System.out.println("aaaaaaaaaaaaaaaaaaaa");
+    }
+}
